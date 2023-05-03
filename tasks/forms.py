e27904428a8d2428543f7b440174dbbe9e90683c
badgersky <szymonek.ptasznik@gmail.com>
2023-05-04 01:29:26 +0200

    remove unused imports

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.core.exceptions import ValidationError
 from . import models
 import datetime
-from users.models import CustomUser
 
 
 class DateInput(forms.DateInput):
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,4 @@
 import datetime
-
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
