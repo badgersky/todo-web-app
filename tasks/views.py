c5b4f40dbd677a8ed565eff825dd5942f595e8c5
badgersky <szymonek.ptasznik@gmail.com>
2023-05-04 02:08:20 +0200

    add styles to pagination

--- tasks/views.py
+++ tasks/views.py
@@ -50,7 +50,7 @@ class DisplayTasks(View):
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
 
-            return render(request, 'tasks/list-tasks.html', {'tasks': page_obj, 'today': today})
+            return render(request, 'tasks/list-tasks.html', {'page_obj': page_obj, 'today': today})
 
         return redirect(reverse('users:login'))
 
